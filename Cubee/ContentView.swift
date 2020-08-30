80b7f1143de672b0549099b879669038a3bb94c7
Michael Schermann <mschermann@users.noreply.github.com>
2020-08-30 12:32:43 -0700

    Initial Commit

---
+++ Cubee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cubee
+//
+//  Created by Michael Schermann on 8/30/20.
+//  Copyright © 2020 Michael Schermann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
